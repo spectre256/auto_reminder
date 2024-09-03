df9dbd058d380df2719b71ff06e5ffbe9350cac7
Ellis Gibbons <egibbons256@gmail.com>
2024-09-03 18:37:17 -0400

    Add license and copyright notices

--- auto_reminder/manual_finder.py
+++ auto_reminder/manual_finder.py
@@ -1,3 +1,25 @@
+# Copyright 2024 Ellis Gibbons
+#
+# Rose-Hulman Institute of Technology, hereby disclaims all copyright interest
+# in the program "auto_reminder" written by Ellis Gibbons.
+#
+# Dr. Jason Yoder 21 May 2024
+# Jason Yoder, Professor
+#
+# This file is part of auto_reminder.
+#
+# auto_reminder is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# auto_reminder is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# auto_reminder. If not, see <https://www.gnu.org/licenses/>.
+
 from auto_reminder.models import StudentFinder, Student, Quiz
 from collections.abc import Iterable
 from functools import partial
